c8c3742af57ec7202bad110af78cb97cab96297f
Ness <149192201+HoatikaNess@users.noreply.github.com>
2024-08-19 00:07:23 +0200

    Create fr.lua

---
+++ fr.lua
@@ -0,0 +1,226 @@
+["fr"] = {
+        ID = "fr",
+
+
+
+        HealthText = "Vie", 
+
+        ArmorText = "Armure", 
+
+        MoneyText = "Argent", 
+
+        HungerText = "Faim",
+
+        License = "License", 
+
+        OwnLicense = "Possède une licence ",
+
+    
+
+        LockdownText = "Retourner chez vous !",
+
+        
+
+        PropText = "Props apparue ", 
+
+       
+
+        WantedText = "Rechercher",
+
+    
+
+        DoorsForSale = "À vendre ",
+
+        SoldDoors = "Vendre porte ",
+
+        PressToBuy = "Appuyer sur F2 pour acheter ",
+
+        GroupDoor = "Groupe de porte ",
+
+        CantBuy = "Ne peut acheter ",
+
+        Access = "accès ",
+
+        Owner = "Propriétaire",
+
+        TeamDoor = "Porte de métier ",
+
+        Unknown = "Inconnu(e)",
+
+        CoOwners = "Co-propriétaire", 
+
+
+
+        YouAreDead = "VOUS ÊTES MORT(E)",
+
+        RespawnIn = "Vous pouvez réapparaître dans :",
+
+        Seconds = "secondes",
+
+        Second = "seconde",
+
+        ReadyToRespawn = "Vous pouvez réapparaître!",
+
+    
+
+        ConnectionLost = "OUPS, CONNEXION PERDUE !",
+
+        ConnectionText = "il semblerait que vous avez perdu la connexion au serveur \n Veuillez attendre le temps que nous essayons d’établir une nouvelle connexion ",
+
+        Disconnect = "Déconnecter", 
+
+        Reconnect = "Re-connecter ",
+
+    
+
+        PlugCharger = "Brancher votre chargeur !",
+
+
+
+        Speed = "Vitesse", 
+
+        Fuel = "Capacité de carburant ", 
+
+        Vehicle = "Véhicule,
+
+
+
+        Licensed = "licencié",
+
+
+
+        YouAreAFK = "Vous êtes AFK", 
+
+        AutoDemote = "Vous allez être automatiquement retiré de votre métier .",
+
+        NoAutoDemote = "Vous n’allez pas être automatiquement retiré de votre métier.",
+
+        SalaryFrozen = "Votre salaire est figé.", 
+
+        CmdToExit = "Écrivez /afk pour quitter le mode afk .",
+
+        
+
+        MayorInitiatedLockdown = "Le maire a initié un couve-feu !",
+
+
+
+        -- CONFIG
+
+        -- CONFIG
+
+        GradientsTitle = "Activer dégradé ",
+
+        GradientsSubtitle = "Activer un dégradé sur l’HUD",
+
+
+
+        PlayerHUDTitle = "Activer l’HUD joueur ",
+
+        PlayerHUDSubtitle = "Activer l’HUD joueur en bas à gauche ",
+
+
+
+        ArmorTitle = "Activer armure",
+
+        ArmorSubtitle = "Activer l’affichage de l’armure en bas à gauche",
+
+
+
+        LockdownTitle = "Activer couvre-feu",
+
+        LockdownSubtitle = "Activer l’affichage du couvre-feu",
+
+
+
+        WeaponHUDTitle = "Activer l’HUD des armes ",
+
+        WeaponHUDSubtitle = "Activer l’HUD des armes en bas à droite ",
+
+
+
+        AgendaTitle = "Activer Agenda",
+
+        AgendaSubtitle = "Activer l’affichage de l’agenda",
+
+
+
+        LawsTitle = "Activer lois", 
+
+        LawsSubtitle = "Activer les lois",
+
+
+
+        WantedTitle = "Activer « rechercher »",
+
+        WantedSubtitle = "Activer les pop-ups d’avis de recherche ",
+
+
+
+        GesturesTitle = "Activer  menu des animations ",
+
+        GesturesSubtitle = "Activer le menu des animations de base du DarkRP",
+
+
+
+        OverheadTitle = "Activer l’hud au dessus de la tête",
+
+        OverheadSubtitle = "Activer l’hud au dessus de la tête",
+
+
+
+        DoorsTitle = "Activer porte",
+
+        DoorsSubtitle = "Activée les informations des portes 3D2D",
+
+
+
+        BatteryTitle = "Activer les alerte battery ",
+
+        BatterySubtitle = "Activer les alerter batterie faibles",
+
+
+
+        VehicleHUDTitle = "Activer l’hud véhicule ",
+
+        VehicleHUDSubtitle = "Activer l’hud en conduisant ",
+
+
+
+        ThemeChange = "Changement de thème", 
+
+        ChooseTheme = "Choisir un thème",
+
+        Save = "Enregistrer", 
+
+        Discard = "Annuler ",
+
+        ApplySettings = "Appliquer les paramètres",
+
+        ChangesNotSaved = "Les changements ne seront pas sauvegarder ",
+
+        
+
+        DarkTheme = "Thème sombre ", 
+
+        WhiteTheme = "Thème claire,
+
+
+
+        Yes = "Oui", 
+
+        No = "Non",
+
+
+
+        OrderedBy = "Ordonné par ",
+
+        Reason = "Raison:", 
+
+        IsWantedFor = "est recherché pour ",
+
+        WarrantIssued = "Search warrant issued for",
+
+        NoLongerWanted = "N’est plus recherché ", 
+
+    },
